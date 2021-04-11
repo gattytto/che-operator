c5a07697dfcc228fabe27d032b323880feccee4e
gattytto <emanaricua@gmail.com>
2021-04-11 03:36:19 +0200

    Update deployment_keycloak.go

--- pkg/deploy/identity-provider/deployment_keycloak.go
+++ pkg/deploy/identity-provider/deployment_keycloak.go
@@ -551,6 +551,7 @@ func GetSpecKeycloakDeployment(
 		" sed -i \"s/127\\.0\\.0\\.1/[\\:\\:1]/g\" /opt/jboss/keycloak/standalone/configuration/standalone.xml" +
 		" && sed -i \"s/\"java.net.preferIPv4Stack\"\\ value=\"true\"/\"java.net.preferIPv4Stack\"\\ value=\"false\"/g\" /opt/jboss/keycloak/domain/configuration/domain.xml" +
 		" && sed -i \"s/230\\.0\\.0\\.4/ff00\\:\\:e600\\:0/g\" /opt/jboss/keycloak/domain/configuration/domain.xml" +
+		" && sed -i \"s/java.net.preferIPv4Stack=true/java.net.preferIPv4Stack=false/g\" /opt/jboss/keycloak/bin/domain.conf" +
 		" && BIND=\"[::1]\" /opt/jboss/docker-entrypoint.sh --debug -b [::1] -c standalone.xml -Djava.net.preferIPv4Stack=false" +
 		" -Dkeycloak.profile.feature.token_exchange=enabled -Dkeycloak.profile.feature.admin_fine_grained_authz=enabled" +
 		" -Djava.net.preferIPv6Addresses=true -Djboss.bind.address.management=\"[::1]\" -Djboss.bind.address.unsecure=\"[::1]\"" +
